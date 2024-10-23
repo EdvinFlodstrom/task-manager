1a6785cc99b578cb82ed0346159256801b7f23a3
Edvin Flodström <edvin.flodstrom@gmail.com>
2024-10-23 20:18:45 +0200

    feat(server/models/task.js): Add task model

--- server/models/task.js
+++ server/models/task.js
@@ -1,26 +1,31 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
-module.exports = (sequelize, DataTypes) => {
-  class Task extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  }
-  Task.init({
-    title: DataTypes.STRING,
-    description: DataTypes.STRING,
-    dueDate: DataTypes.DATE,
-    completed: DataTypes.BOOLEAN
-  }, {
-    sequelize,
-    modelName: 'Task',
-  });
-  return Task;
+
+import { Model } from 'sequelize';
+
+export default (sequelize, DataTypes) => {
+        class Task extends Model {
+                /**
+                 * Helper method for defining associations.
+                 * This method is not a part of Sequelize lifecycle.
+                 * The `models/index` file will call this method automatically.
+                 */
+                static associate(models) {
+                        // define association here
+                }
+        }
+
+        Task.init(
+                {
+                        title: DataTypes.STRING,
+                        description: DataTypes.STRING,
+                        dueDate: DataTypes.DATE,
+                        completed: DataTypes.BOOLEAN,
+                },
+                {
+                        sequelize,
+                        modelName: 'Task',
+                },
+        );
+
+        return Task;
 };
