37af7a24c7ccfbe195cf3c3023044cdc9f361dc5
Edvin Flodström <edvin.flodstrom@gmail.com>
2024-10-26 14:28:47 +0200

    feat(client/src/redux/taskSlice): Add taskSlice actions

--- client/src/redux/taskSlice/actions.js
+++ client/src/redux/taskSlice/actions.js
@@ -1,2 +1,41 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
+
+const tasksHttpAddress = 'http://localhost:3001/tasks';
+
+export const fetchTasks = createAsyncThunk('tasks/fetchTasks', async () => {
+        const response = await axios.get(tasksHttpAddress);
+        return response.data;
+});
+
+export const addTask = createAsyncThunk('tasks/addTask', async (task) => {
+        const response = await axios.post(tasksHttpAddress, task);
+        return response.data;
+});
+
+export const updateTaskCompletion = createAsyncThunk(
+        'tasks/updateTaskCompletion',
+        async ({ id, completed }) => {
+                const response = await axios.patch(
+                        `${tasksHttpAddress}/${id}/completed`,
+                        { completed },
+                );
+                return response.data;
+        },
+);
+
+export const updateTaskDetails = createAsyncThunk(
+        'tasks/updateTaskDetails',
+        async ({ id, details }) => {
+                const response = await axios.patch(
+                        `${tasksHttpAddress}/${id}/details`,
+                        details,
+                );
+                return response.data;
+        },
+);
+
+export const deleteTask = createAsyncThunk('tasks/deleteTask', async (id) => {
+        const response = await axios.delete(`${tasksHttpAddress}/${id}`);
+        return response.data;
+});
--- client/src/redux/taskSlice/reducer.js
+++ client/src/redux/taskSlice/reducer.js
@@ -1,5 +1,12 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-const taskSlice = createSlice({});
+const taskSlice = createSlice({
+        name: 'tasks',
+        initialState: {
+                tasks: [],
+                loading: false,
+                error: null,
+        },
+});
 
 export default taskSlice.reducer;
