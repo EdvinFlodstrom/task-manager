d88a6f711aacd483af40de07b155235950981a9c
Edvin Flodström <edvin.flodstrom@gmail.com>
2024-10-26 15:11:11 +0200

    feat(client/src/redux/taskSlice): Add selector logic

---
+++ client/src/redux/taskSlice/selectors.js
@@ -0,0 +1,3 @@
+export const selectAllTasks = (state) => state.tasks.tasks;
+export const selectLoadingStatus = (state) => state.tasks.loading;
+export const selectError = (state) => state.tasks.error;
