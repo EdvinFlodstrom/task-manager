aa6ee2061a8381cb6e1779977a9bae0bcdbba113
Edvin Flodström <edvin.flodstrom@gmail.com>
2024-10-27 11:06:26 +0100

    chore(client/src/App.jsx): Add initial task display

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,18 @@
+import React from 'react';
+import TaskList from './components/TaskList';
 import './css/App.css';
 
 const App = () => {
         return (
                 <div className='app-container'>
                         <h1>Task Manager</h1>
+                        <button
+                                className='fetch-tasks-button'
+                                onClick={() => window.location.reload()}
+                        >
+                                Refresh Tasks
+                        </button>
+                        <TaskList />
                 </div>
         );
 };
