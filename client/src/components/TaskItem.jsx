f5df79c7db54c12006a76a82f5d9aaef34cd680d
Edvin Flodström <edvin.flodstrom@gmail.com>
2024-10-27 11:05:33 +0100

    feat(client/src/components): Add TaskItem.jsx logic

---
+++ client/src/components/TaskItem.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { updateTaskCompletion, deleteTask } from '../redux/taskSlice/actions';
+
+const TaskItem = ({ task }) => {
+        const dispatch = useDispatch();
+
+        const handleToggleComplete = () => {
+                dispatch(
+                        updateTaskCompletion({
+                                id: task.id,
+                                completed: !task.completed,
+                        }),
+                );
+        };
+
+        const handleDelete = () => {
+                dispatch(deleteTask(task.id));
+        };
+
+        return (
+                <div className='task-item'>
+                        <button
+                                className='task-delete-button'
+                                onClick={handleDelete}
+                        >
+                                Delete
+                        </button>
+                        <div className='task-info'>
+                                <h3>{task.title}</h3>
+                                <p>{task.description}</p>
+                                <p>Due: {task.dueDate}</p>
+                        </div>
+                        <input
+                                type='checkbox'
+                                className='task-completed-checkbox'
+                                checked={task.completed}
+                                onChange={handleToggleComplete}
+                                style={{
+                                        backgroundColor: task.completed
+                                                ? `green`
+                                                : `red`,
+                                }}
+                        />
+                </div>
+        );
+};
+
+export default TaskItem;
