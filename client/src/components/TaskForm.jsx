66d6faa7e1b057a1d0519ce3f7a54d6bd43f8cc3
Edvin Flodström <edvin.flodstrom@gmail.com>
2024-10-29 13:08:37 +0100

    feat(client/src/components/TaskForm.jsx): Add option to include due time

--- client/src/components/TaskForm.jsx
+++ client/src/components/TaskForm.jsx
@@ -8,22 +8,44 @@ const TaskForm = ({ toggleForm }) => {
         const [title, setTitle] = useState('');
         const [description, setDescription] = useState('');
         const [dueDate, setDueDate] = useState('');
+        const [dueTime, setDueTime] = useState('');
 
         const dispatch = useDispatch();
+
+        // Function to combine the due date and time, or use the one that is not empty.
+        const formatDateAndTime = (date, time) => {
+                if (!date && !time) {
+                        return '';
+                } else if (date && time) {
+                        return new Date(`${dueDate}T${dueTime}`).toISOString();
+                } else if (dueDate && !dueTime) {
+                        return new Date(`${dueDate}`).toISOString();
+                } else {
+                        // Get the current date in 'yyyy-mm-dd' format
+                        const today = new Date().toISOString().split('T')[0];
+                        return new Date(`${today}T${dueTime}`).toISOString();
+                }
+        };
 
         const handleSubmit = (e) => {
                 e.preventDefault();
                 if (title) {
+                        const dueDateAndTime = formatDateAndTime(
+                                dueDate,
+                                dueTime,
+                        );
+
                         dispatch(
                                 addTask({
                                         title,
                                         description,
-                                        dueDate,
+                                        dueDate: dueDateAndTime,
                                 }),
                         );
                         setTitle('');
                         setDescription('');
                         setDueDate('');
+                        setDueTime('');
 
                         // Hide form after submitting task
                         toggleForm();
@@ -48,6 +70,11 @@ const TaskForm = ({ toggleForm }) => {
                                 type='date'
                                 value={dueDate}
                                 onChange={(e) => setDueDate(e.target.value)}
+                        />
+                        <input
+                                type='time'
+                                value={dueTime}
+                                onChange={(e) => setDueTime(e.target.value)}
                         />
 
                         <button type='submit' className='primary-button'>
