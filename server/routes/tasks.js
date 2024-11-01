21ceb06e29b7518b22b226fcfbc5abaeb5ef8592
Edvin Flodström <edvin.flodstrom@gmail.com>
2024-11-01 10:34:47 +0100

    fix(server/routes/tasks.js): Remove unnecessary (and unfunctional) timezone conversion

--- server/routes/tasks.js
+++ server/routes/tasks.js
@@ -13,48 +13,10 @@ const { Task } = db;
 
 const router = express.Router();
 
-// Helper function to convert from UTC to requested timezone
-const convertTasksToTimezone = (tasks, timezone) => {
-        if (!timezone) return tasks;
-
-        const tasksArray = Array.isArray(tasks) ? tasks : [tasks];
-
-        // TODO: Find proper solution to convert from UTC to given timezone
-        // This solution accurately translates the time, but keeps the 'Z' that marks it as a UTC time, which is wrong
-        // date-fns-tz did not seem to manage Swedish timezones well
-        const convertedTasks = tasksArray.map((task) => {
-                // Convert UTC date to local time in specified timezone with correct offset
-                const utcDate = new Date(task.dueDate);
-                const offsetMinutes = utcDate.getTimezoneOffset();
-
-                // Adjust the date by the offset to account for DST changes
-                const localDate = new Date(
-                        utcDate.getTime() - offsetMinutes * 60 * 1000,
-                );
-                const formattedDate = localDate.toLocaleString('en-US', {
-                        timeZone: timezone,
-                        hour12: false,
-                        year: 'numeric',
-                        month: '2-digit',
-                        day: '2-digit',
-                        hour: '2-digit',
-                        minute: '2-digit',
-                        second: '2-digit',
-                });
-
-                task.dueDate = formattedDate;
-                return task;
-        });
-
-        return Array.isArray(tasks) ? convertedTasks : convertedTasks[0];
-};
-
 router.get('/', async (req, res) => {
-        const { timezone } = req.query;
-
         try {
                 const tasks = await Task.findAll();
-                res.json(convertTasksToTimezone(tasks, timezone));
+                res.json(tasks);
         } catch (error) {
                 res.status(500).json({
                         message: 'Error fetching tasks',
@@ -68,7 +30,6 @@ router.post(
         validateCreateTask,
         handleValidationErrors,
         async (req, res) => {
-                const { timezone } = req.query;
                 const { title, description, dueDate } = req.body;
 
                 try {
@@ -78,9 +39,7 @@ router.post(
                                 dueDate,
                                 completed: false,
                         });
-                        res.status(201).json(
-                                convertTasksToTimezone(task, timezone),
-                        );
+                        res.status(201).json(task);
                 } catch (error) {
                         res.status(500).json({
                                 message: 'Error creating task',
@@ -96,7 +55,6 @@ router.patch(
         validateUpdateTaskCompleted,
         handleValidationErrors,
         async (req, res) => {
-                const { timezone } = req.query;
                 const { id } = req.params;
                 const { completed } = req.body;
 
@@ -105,9 +63,7 @@ router.patch(
                         if (task) {
                                 task.completed = completed;
                                 await task.save();
-                                res.json(
-                                        convertTasksToTimezone(task, timezone),
-                                );
+                                res.json(task);
                         } else {
                                 res.status(404).send('Task not found');
                         }
@@ -126,7 +82,6 @@ router.patch(
         validateUpdateTaskDetails,
         handleValidationErrors,
         async (req, res) => {
-                const { timezone } = req.query;
                 const { id } = req.params;
                 const { dueDate, description } = req.body;
 
@@ -136,9 +91,7 @@ router.patch(
                                 if (dueDate) task.dueDate = dueDate;
                                 if (description) task.description = description;
                                 await task.save();
-                                res.json(
-                                        convertTasksToTimezone(task, timezone),
-                                );
+                                res.json(task);
                         } else {
                                 res.status(404).send('Task not found');
                         }
@@ -156,14 +109,12 @@ router.delete(
         validateDeleteTask,
         handleValidationErrors,
         async (req, res) => {
-                const { timezone } = req.query;
                 const { id } = req.params;
 
                 try {
                         let task = await Task.findByPk(id);
                         if (task) {
                                 await task.destroy();
-                                task = convertTasksToTimezone(task, timezone);
                                 res.json({
                                         message: 'Task deleted',
                                         task: task,
